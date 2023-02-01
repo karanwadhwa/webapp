54ba7ccd5ea157c1529f6748efa2e792b92de576
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-01 01:57:30 -0500

    break down index into app and server

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const app = express();
-const PORT = "3000";
 
 // Middleware
 app.use(express.json({ extended: true }));
@@ -14,4 +13,4 @@ app.get("/healthz", (req, res) => {
 // Route definitions
 app.use("/v1/user", require("./src/routes/user"));
 
-app.listen(PORT, () => console.log(`server started on http://localhost:${PORT}`));
+module.exports = app;
