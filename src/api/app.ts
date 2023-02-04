9eabe665d2c290d5038260441c61a712d6456329
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-04 03:46:15 -0500

    UserController and create user route

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,5 +1,6 @@
 // import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
+import UserRouter from "./routes/UserRoutes";
 
 const app: Application = express();
 
@@ -10,5 +11,7 @@ app.use(express.json());
 app.get("/healthz", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
+
+app.use("/v1/user", UserRouter);
 
 export default app;
