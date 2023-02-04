954e9f3b9c109833f9a26a3868c6cb32f0635dfd
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-04 00:09:42 -0500

    database initialization and User model setup

---
+++ src/api/models/index.ts
@@ -0,0 +1,45 @@
+import { Sequelize } from "sequelize";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import User from "./User";
+dotenv.config();
+
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+interface dbInterface {
+  User: (s: Sequelize) => Sequelize;
+}
+
+let db: dbInterface;
+
+export async function initializeDatabase() {
+  // Create Database if it doesnt already exist
+  const connection = mysql.createConnection(dbConfig);
+  connection.query(`CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;`, (err, result) => {
+    if (err) throw err;
+    console.log("database created", result);
+
+    // initialize db connection with sequelize
+    const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+      host: DB_HOST,
+      dialect: "mysql",
+    });
+
+    db = {
+      User: User(sequelize),
+    };
+
+    return sequelize.sync({ alter: true });
+  });
+}
+
+export default db;
