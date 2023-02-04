b4af01c7b922704eae5ae5188ddec7d868bf39a0
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-04 03:52:14 -0500

    create user validation

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { validationResult } from "express-validator";
 import UserService from "../services/UserService";
 
 const userService = new UserService();
@@ -7,13 +8,15 @@ class UserController {
   constructor() {}
 
   createUser = async (req: express.Request, res: express.Response): Promise<express.Response> => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty())
+      return res.status(400).json({ errors: validationErrors.array() });
+
     const user = await userService.findByUsername(req.body.username);
     if (user)
       return res.status(400).json({ error: "An account with that username already exists" });
 
     let created = await userService.create(req.body);
-    // created = created.toJSON();
-    // delete created.password;
     return res.status(200).json({ user: created });
   };
 }
