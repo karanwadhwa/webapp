993cc79379fc1f158c127daf71762d0153a1c5c0
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-04 03:52:14 -0500

    create user validation

--- src/api/routes/UserRoutes.ts
+++ src/api/routes/UserRoutes.ts
@@ -1,4 +1,5 @@
 import * as express from "express";
+import { check } from "express-validator";
 import UserController from "../controllers/UserController";
 
 const router = express.Router();
@@ -15,6 +16,14 @@ router.get("/", (req: express.Request, res: express.Response) => {
 // @desc    Create new user
 // @access  Public
 // @response codes  201, 400
-router.post("/", controller.createUser);
+router.post(
+  "/",
+  check("first_name", "first_name is a required field").not().isEmpty(),
+  check("last_name", "last_name is a required field").not().isEmpty(),
+  check("username", "username should be a valid email address").isEmail(),
+  check("password", "password is a required field").not().isEmpty(),
+  check("password", "password needs to be 6 characters or longer").isLength({ min: 6 }),
+  controller.createUser
+);
 
 export default router;
