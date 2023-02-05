cd8e6150d1e6ef73d6a2c29093ac0eb052a5505c
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-04 20:28:50 -0500

    delete old js files

--- srcjs/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-const PORT = process.env.API_PORT || "3000";
-
-// Route definitions
-app.use("/v1/user", require("./src/routes/user"));
-
-app.listen(PORT, () => console.log(`server started on http://localhost:${PORT}`));
