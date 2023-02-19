e232b30dc7bd6e9c5fd7ecef0759b5a31eb44d23
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-19 01:22:32 -0500

    packer scripts

---
+++ src/infra/app.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# packer recommended - wait for ec2 instance to fully setup
+sleep 30
+
+# install updates
+sudo yum update -y
+
+# install node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 16 -y
+
+# install mysql
+sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm -y
+sudo yum repolist
+sudo amazon-linux-extras install epel -y
+sudo yum -y install mysql-community-server
+sudo systemctl enable --now mysqld
+systemctl status mysqld
+# sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF;}'
+ROOT_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF;}')
+# MYSQL_PASSWORD='Mysql@6225'
+echo "${ROOT_PASSWORD}"
+sudo mysql -u "root" --password="${ROOT_PASSWORD}" --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Mysql@6225';"
+
+# copy and unzip repo
+sudo yum install unzip -y
+cd ~/ && unzip webapp.zip
+cd ~/webapp && npm install
+npm run build
+echo -e "API_PORT=3000\nDB_HOST=localhost\nDB_USER=root\nDB_PASSWORD=Mysql@6225\nDB_DATABASE=webapp6225" > .env
+
+
+# move systemd service file
+sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
