f3945d770db8a7756eb17ca6941b21dfb4e7ef4b
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-03-19 04:20:35 -0400

    update packer scripts for cloudwatch config

--- src/infra/app.sh
+++ src/infra/app.sh
@@ -11,28 +11,34 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16 -y
 
-# # install mysql
-# sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm -y
-# sudo yum repolist
-# sudo amazon-linux-extras install epel -y
-# sudo yum -y install mysql
-# sudo systemctl enable --now mysqld
-# systemctl status mysqld
-# # sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF;}'
-# ROOT_PASSWORD=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF;}')
-# echo "${ROOT_PASSWORD}"
-# sudo mysql -u "root" --password="${ROOT_PASSWORD}" --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
-
 # copy and unzip repo
 sudo yum install unzip -y
 cd ~/ && unzip webapp.zip
 cd ~/webapp && npm install
 npm run build
-# echo "$DB_PASSWORD"
-# echo -e "API_PORT=3000\nDB_HOST=localhost\nDB_USER=root\nDB_PASSWORD=$DB_PASSWORD\nDB_DATABASE=webapp6225" > .env
-# cat .env
+
+# install cloudwatch agent
+sudo yum install -y amazon-cloudwatch-agent
 
 # move systemd service file
 sudo mv /tmp/webapp.service /etc/systemd/system/webapp.service
+# move cloudwatch config file
+sudo mv /tmp/cloudwatch-config.json /opt/cloudwatch-config.json
+# move rsyslog config file
+sudo mv /tmp/csye6225.conf /etc/rsyslog.d/csye6225.conf
+
+# create csye6225.log file
+sudo touch /var/log/csye6225.log
+
+# reload systemctl daemon
+sudo systemctl daemon-reload
+sudo systemctl restart rsyslog.service
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+
 # sudo systemctl enable webapp.service
 # sudo systemctl start webapp.service
