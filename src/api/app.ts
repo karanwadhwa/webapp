1d717ebecfc6cf32894670e2c84477061aed1d5e
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-03-19 03:15:53 -0400

    setup winston for express logging

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,12 +1,35 @@
 // import dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
+import winston from "winston";
+import morgan from "morgan";
+
 import UserRouter from "./routes/UserRoutes";
 import ProductRouter from "./routes/ProductRoutes";
 
 const app: Application = express();
 
+const { combine, timestamp, json } = winston.format;
+const logger = winston.createLogger({
+  level: "http",
+  format: combine(
+    timestamp({
+      format: "YYYY-MM-DD hh:mm:ss.SSS A",
+    }),
+    json()
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+const morganMiddleware = morgan(
+  ":method :url :status :res[content-length] - :response-time ms",
+  {
+    write: (message: String): winston.Logger => logger.http(message.trim()),
+  }
+);
+
 // Middleware
 app.use(express.json());
+app.use(morganMiddleware);
 
 // Health check endpoint
 app.get("/healthz", (req: Request, res: Response) => {
