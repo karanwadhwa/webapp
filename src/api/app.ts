d9e5df365a355389b648910aa22ee4cf5802ebb1
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-03-20 21:26:07 -0400

    log statsD for every api call

--- src/api/app.ts
+++ src/api/app.ts
@@ -2,11 +2,13 @@
 import express, { Application, Request, Response } from "express";
 import winston from "winston";
 import morgan from "morgan";
+import statsD from "node-statsd";
 
 import UserRouter from "./routes/UserRoutes";
 import ProductRouter from "./routes/ProductRoutes";
 
 const app: Application = express();
+const statsdClient: statsD = new statsD();
 
 const { combine, timestamp, json } = winston.format;
 const logger = winston.createLogger({
@@ -27,9 +29,15 @@ const morganMiddleware = morgan(
   }
 );
 
+const statsDMiddleware = (req, res, next) => {
+  statsdClient.increment(`${req.method} ${req.url}`);
+  next();
+};
+
 // Middleware
 app.use(express.json());
 app.use(morganMiddleware);
+app.use(statsDMiddleware);
 
 // Health check endpoint
 app.get("/healthz", (req: Request, res: Response) => {
